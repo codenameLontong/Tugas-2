5be42f0c586cfa4deaba7ea97256f3a8d0ad42bc
Jeremy Mervin <jeremymervin14@gmail.com>
2022-09-15 00:56:14 +0700

    Menambahkan fungsi, routing, load data pada views, url, html agar dapat menampilkan data

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+# Create your views here.
+def show_katalog(request):
+    data_catalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_catalog,
+        'nama': 'Mervin',
+        'id' : '2106654675'
+    }
+    return render(request, "katalog.html", context)
