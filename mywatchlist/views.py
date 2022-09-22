61df914e46f395dcde7d1264f3f95dbbe985131c
Jeremy Mervin <jeremymervin14@gmail.com>
2022-09-22 11:32:59 +0700

    Added new function

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -29,3 +29,12 @@ def show_json_by_id(request, id):
 def show_xml_by_id(request, id):
     data = Movies.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_item_by_id(request):
+    data_watchlist = Movies.objects.filter(pk=id)
+    context = {
+        'list': data_watchlist,
+        'nama': 'Jeremy Mervin',
+        'id' : '2106654675',
+    }
+    return render(request, "mywatchlist.html", context)
