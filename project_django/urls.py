7dd6bc4821501a47ef218538daa29595e8dca911
codenameLontong <100303130+codenameLontong@users.noreply.github.com>
2022-09-25 12:46:03 +0700

    Update urls.py
    Failure app, fixed some features for demo

--- project_django/urls.py
+++ project_django/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mywatchlist.urls')),
+    path('mywatchlist/', include('mywatchlist.urls')),
+    path('katalog/', include('katalog.urls')),
 ]
 
 # TODO: Implement Routings Here
