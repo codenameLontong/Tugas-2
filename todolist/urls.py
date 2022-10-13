83424f244d6d9cd4ec36eb81d9346ab6c73bd19a
Jeremy Mervin <jeremymervin14@gmail.com>
2022-10-13 11:02:24 +0700

    Added BONUS

--- todolist/urls.py
+++ todolist/urls.py
@@ -10,8 +10,9 @@ urlpatterns = [
 	path('create-task/', create_task, name='create_task'),
 	path('logout/', logout_user, name='logout'),
 	path('finished/<int:pk>', finish_task, name='finished'),
-	path('delete/ <int:pk>', delete_task, name='delete'),
+	path('deleted/ <int:pk>', delete_task, name='delete'),
 	path('delete/<int:id>', delete_task_ajax, name='delete_task_ajax'),
     path('json/', get_todolist_json, name="get_todolist_json"),
     path('add/', create_task_json, name="create_task_json"),
+	# path('delete-task/<int:pk>', delete, name='delete_ajax'),
 ]
